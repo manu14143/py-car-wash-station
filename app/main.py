28b60b2bfd07722f74e20a1b89ea88da5bbc7102
Ram Rachakonda <manu14143@gmail.com>
2026-01-05 17:17:00 +1100

    updated script

--- app/main.py
+++ app/main.py
@@ -24,16 +24,16 @@ class CarWashStation:
                            / self.distance_from_city_center)
         return round(income, 1)
 
-    def calculate_washing_price(self, car) -> float:
+    def calculate_washing_price(self, car: list) -> float:
         price = (car.comfort_class * (self.clean_power - car.clean_mark)
                  * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car) -> None:
+    def wash_single_car(self, car: list) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating) -> None:
+    def rate_service(self, rating: int) -> None:
         self.average_rating = round(((self.average_rating
                                     * self.count_of_ratings) + rating)
                                     / (self.count_of_ratings + 1), 1)
