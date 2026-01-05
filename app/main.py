f5f2b8a6531f5dd947e4548c88e814e1ed0e7d5a
Ram Rachakonda <manu14143@gmail.com>
2026-01-05 17:08:13 +1100

    updated script

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
+            -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int ) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -17,11 +19,15 @@ class CarWashStation:
         income = 0
         for j in car:
             if j.clean_mark < self.clean_power:
-                income += j.comfort_class * ( self.clean_power - j.clean_mark) * self.average_rating / self.distance_from_city_center
+                income += (j.comfort_class
+                           * ( self.clean_power - j.clean_mark)
+                           * self.average_rating
+                           / self.distance_from_city_center)
         return round(income, 1)
 
     def calculate_washing_price(self, car):
-        price = car.comfort_class * ( self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+        price = (car.comfort_class * ( self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car):
@@ -29,5 +35,7 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating):
-        self.average_rating = round(((self.average_rating * self.count_of_ratings) + rating) / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(((self.average_rating
+                                    * self.count_of_ratings) + rating)
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
